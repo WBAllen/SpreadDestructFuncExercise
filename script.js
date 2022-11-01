1dc29bc4b87c61728b72cd0d7fca5ed1f2f8469e
Ben Allen <live.live.live.island@gmail.com>
2022-11-01 10:15:11 -0400

    new homework

---
+++ script.js
@@ -0,0 +1,55 @@
+// Spread & Destructuring with Functions Exercise
+
+// 1a.
+const jPDinos = [
+    "Velociraptors", 
+    "Tyrannosaurus Rex", 
+    "Dilophosaurus"
+]
+
+// 1b.
+function seeDinos(a,...b){
+    console.log(a);
+    console.log(b);
+}
+seeDinos("Velociraptors", "Tyrannosaurus Rex", "Dilophosaurus");
+
+// 2a.
+const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+};
+
+// 2b.
+seeCharacters = (jPCharacters) => {
+    console.log(jPCharacters.ellieSattler);
+}
+seeCharacters(jPCharacters);
+
+// BONUS
+// 3a.
+const jurassicParkMovies = [{
+    one: "Jurassic Park", 
+    two: "The Lost World: Jurassic Park",
+    three: "Jurassic Park III"
+},
+{
+    four: "Jurassic World", 
+    five: "Jurassic World: Fallen Kingdom", 
+    six: "Jurassic World: Dominion"
+}];
+
+// 3b.
+const seeJPMovies = function([{one, two, three}, {four, five, six}]){
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+    console.log(five);
+    console.log(six);
+}
+seeJPMovies(jurassicParkMovies);
+
+
+// When finished, create a repository in the “SpreadDestructFuncExercise” Folder as well as on GitHub. Add and commit everything in the SpreadDestructFuncExercise folder and push it up to your GitHub repository. Then paste the link to your GitHub repository page in the form in google classroom.
